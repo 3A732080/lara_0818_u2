4897341a9a238654e102ad2ac0fd43e5c5ffafa0
3A732080 <tiffany89031176@gmail.com>
2020-11-01 22:59:17 +0800

    建立子樣板

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home',function(){
-    return view('home.index');
+Route::get('/home',function(){
+    return view('home');
 });
 
